6546ae15b442e2dbf2976e17453bb658d89ce736
Rincevent42 <porvooc@gmail.com>
2020-06-09 08:20:31 +0200

    tests written

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,15 @@
+// capitalizeFirstLetters.js
+const assert = require('assert');
+
+// WRITE THE ACTUAL FUNCTION HERE
+
+// Check that capitalizeFirstLetters is a function
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+// Check that capitalizeFirstLetters accepts one argument
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+// Check that capitalizeFirstLetters transforms a sentence correctly
+assert.strictEqual(capitalizeFirstLetters('nous sommes le 9 juin et il pleut.'), 'Nous Sommes Le 9 Juin Et Il Pleut.');
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
